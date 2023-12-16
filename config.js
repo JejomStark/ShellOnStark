ca88a8dec40f04b7621674ab43447d7c62af8bed
JejomStark <jp@nodeguardians.io>
2023-12-16 23:00:19 +0100

    chore: added the file to save scheduled swap

--- config.js
+++ config.js
@@ -5,10 +5,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = {
     files: { 
-        tokens_list: path.join(__dirname, 'files', 'personal_tokens.json')
+        tokens_list: path.join(__dirname, 'files', 'personal_tokens.json'),
+        scheduled_swap: path.join(__dirname, 'files', 'scheduled_swap.json'),
+        executed_swap: path.join(__dirname, 'files', 'executed_swap.json')
     },
     swap: {
-        slippage: 0.01 //1%
+        slippage: 0.01, //1%
+        max_gas_fees: 2 //2$
     }
 };
 
