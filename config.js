880beb954b8ce9042ea7b9ede123c4523b18e981
JejomStark <jp@nodeguardians.io>
2023-12-18 21:55:12 +0100

    chore: plan swap feature

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const config = {
     files: { 
         tokens_list: path.join(__dirname, 'files', 'personal_tokens.json'),
         scheduled_swap: path.join(__dirname, 'files', 'scheduled_swap.json'),
-        executed_swap: path.join(__dirname, 'files', 'executed_swap.json')
+        executed_swap: path.join(__dirname, 'files', 'executed_swap.json'),
     },
     swap: {
-        slippage: 0.01, //1%
-        max_gas_fees: 2 //2$
+        slippage: 0.03, //1%
+        max_gas_fees_in_usd: 1 //1$
     }
 };
 
